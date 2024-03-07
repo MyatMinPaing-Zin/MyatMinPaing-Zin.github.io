49f6ea53579785a258cc4ac3d150c05bbbfd153e
Kio <myatminpaingzin94515@gmail.com>
2024-03-07 17:55:50 +0800

    add cv

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function downloadPDF() {
+    // URL to your PDF file
+    var pdfURL = 'Myat_Min_Paing_Zin_resume.pdf';
+
+    // Create a temporary link element
+    var link = document.createElement('a');
+    link.href = pdfURL;
+
+    // Set the download attribute with the filename you want
+    link.download = 'Myat_Min_Paing_Zin_resume.pdf';
+
+    // Programmatically click on the link to initiate download
+    link.click();
+}
